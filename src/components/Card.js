4091bf4be9a1b509177875efbbc41f5bf6a5424f
Samkit Barbhaya <samkitbarbhaya@Samkits-MacBook-Air.local>
2023-01-14 14:05:36 -0700

    second commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ export default function Card(props){
                 <div className="card--location">
                     <img src="./images/gps.jpg" alt="" className="gps-icon"></img>
                     <span className="card--country">{props.location}</span>
-                    <a href={props.googleMapsUrl} target="_blank"><span className="card--maplink">View on Google Maps</span></a>
+                    <a href={props.googleMapsUrl} target="_blank" rel="noreferrer"><span className="card--maplink">View on Google Maps</span></a>
                 </div>
                 <p className="card--title">{props.title}</p>
                 <p className="card--dates">{props.startDate} - {props.endDate}</p>
