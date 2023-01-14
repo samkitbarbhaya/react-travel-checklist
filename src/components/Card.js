c845e8f39c6f800ac05753fe83da179f529c2d01
Samkit Barbhaya <samkitbarbhaya@Samkits-MacBook-Air.local>
2023-01-14 13:59:52 -0700

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Card(props){
+    return (
+        <div className="card">
+            <img className="card--img" src={props.imageUrl} alt=""></img>
+            <div className="card--info">
+                <div className="card--location">
+                    <img src="./images/gps.jpg" alt="" className="gps-icon"></img>
+                    <span className="card--country">{props.location}</span>
+                    <a href={props.googleMapsUrl} target="_blank"><span className="card--maplink">View on Google Maps</span></a>
+                </div>
+                <p className="card--title">{props.title}</p>
+                <p className="card--dates">{props.startDate} - {props.endDate}</p>
+                <p className="card--description">{props.description}</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <nav>
+            <img src="./images/globe-24.png"></img>
+            <h1>My Travel Journal</h1>
+        </nav>
+    )
+}
