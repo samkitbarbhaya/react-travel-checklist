69e070cbcb1a9991c94b59b982ce4371c8f6210d
Samkit Barbhaya <samkitbarbhaya@Samkits-MacBook-Air.local>
2023-01-14 14:08:28 -0700

    third commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar(){
     return(
         <nav>
-            <img src="./images/globe-24.png"></img>
+            <img src="./images/globe-24.png" alt=""></img>
             <h1>My Travel Journal</h1>
         </nav>
     )
